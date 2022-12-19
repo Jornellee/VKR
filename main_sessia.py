ed4637399ba15fd586dc5fa55811dcc9aba8cc0a
Jornellee <102516207+Jornellee@users.noreply.github.com>
2022-12-19 15:47:20 +0800

    Реализовани оповещения о сессии

--- main_sessia.py
+++ main_sessia.py
@@ -13,32 +13,63 @@ TAG_RE = re.compile(r'<[^>]+>')
 def remove_tags(text):
     return TAG_RE.sub('', text)
 
-def get_changes(GROUPS_TO_TELEGRAMS_IDS, TEACHERS_TO_TELEGRAMS_IDS):
+def get_sessia(GROUPS_TO_TELEGRAMS_IDS, TEACHERS_TO_TELEGRAMS_IDS):
     # сессия - сеанс подключения к БД
     s = Session()
 
     today = pendulum.now()   
-    d1 = today.add(weeks=1).start_of("week").start_of("day")
+    d0 = today.start_of("week").start_of("day")
+    d1 = d0.add(weeks=1)
     d2 = d1.add(weeks=1)
-
-    #скрипт о начале сессии
-    q =  text(f"""
-    SELECT unnest(groups)
-    , array_agg (DISTINCT dbeg)
-    FROM schedule_v2
-    WHERE dbeg in (:x, :y)
-    and type = 'day'
-    GROUP BY unnest(groups) 
-    """)
-
-    rows = s.execute (q, {"x": d1, "y": d2})
-
-    rows = list(rows)
 
     groups_info = {}
     teachers_info = {}
 
-    telegram_ids = TEACHERS_TO_TELEGRAMS_IDS.get(teacher_id, [])
+    #скрипт о начале сессии для студентов
+    q =  text(f"""
+    SELECT unnest(groups) id
+    , array_agg (DISTINCT dbeg) weeks 
+    FROM schedule_v2
+    WHERE dbeg in (:x, :y, :z)
+    and type = 'day'
+    GROUP BY unnest(groups) 
+    """)
+    groups_rows = s.execute (q, {"x": d1, "y": d2, "z":d0})
+
+    groups_rows = list(groups_rows)
+
+    for row in groups_rows:
+        if len(row.weeks) == 2:
+            groups_info [row.id] = "Следующая неделя зачетная"
+        elif len(row.weeks) == 1:
+            groups_info [row.id] = f" {row.id} Следующая неделя экзаменационная и пар не будет"
+
+
+    #скрипт о начале сессии для преподавателей
+    q =  text(f"""
+    SELECT unnest(teachers) id
+    , array_agg (DISTINCT dbeg) weeks
+    FROM schedule_v2
+    WHERE dbeg in (:x, :y, :z)
+    and type = 'day'
+    GROUP BY unnest(teachers) 
+    """)
+    teachers_rows = s.execute (q, {"x": d1, "y": d2, "z": d0})
+
+    teachers_rows = list(teachers_rows)
+
+    for row in teachers_rows:
+        if len(row.weeks) == 0:
+            teachers_info [row.id] = f"{row.id} На следующей неделе пар не будет, проверьте расписание в личном кабинете"
+
 
         # рассылаем сообщения о начале сессии
+    for group_id in groups_info:
+        message = groups_info[group_id]
+        # вытащили список подписчиков данной группы group_id
+        telegram_ids = GROUPS_TO_TELEGRAMS_IDS.get(group_id, [])
+        # рассылаем сообщения о начале сессии
         for user_id in telegram_ids:
+             bot.send_message (user_id, message, parse_mode='HTML')
+
+get_sessia(GROUPS_TO_TELEGRAMS_IDS, TEACHERS_TO_TELEGRAMS_IDS)
